0d2f19e2ddfda2d8a55b909ea5b398c672a3eee5
profrontdeveloper604 <159055857+profrontdeveloper604@users.noreply.github.com>
2024-02-16 16:58:10 +0330

    Add files via upload

---
+++ assets/js/faq.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const questions = document.querySelectorAll(".question");
+
+  questions.forEach((question) => {
+    const answer = question.nextElementSibling;
+    const toggleIcon = question.querySelector(".icon");
+    const toggleIconImg = question.querySelector(".icon img");
+    question.addEventListener("click", () => {
+      answer.classList.toggle("show");
+      toggleIconImg.setAttribute(
+        "src",
+        answer.classList.contains("show")
+          ? "../images/icon-minus.svg"
+          : "../images/icon-plus.svg"
+      );
+    });
+    toggleIcon.addEventListener("click", (event) => {
+      event.stopPropagation();
+      answer.classList.toggle("show");
+      toggleIconImg.setAttribute(
+        "src",
+        answer.classList.contains("show")
+          ? "/assets/images/icon-minus.svg"
+          : "/assets/images/icon-plus.svg"
+      );
+    });
+  });
+});
